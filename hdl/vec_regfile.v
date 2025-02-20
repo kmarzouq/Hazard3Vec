d5c0de226d522a57bd4389e1b0510b94e1d7d049
kmarzouq <marzouq.mujtaba@gmail.com>
2025-02-20 00:44:28 -0800

    confusion

--- hdl/vec_regfile.v
+++ hdl/vec_regfile.v
@@ -3,15 +3,15 @@ module Vec_RF (clk, reset, RegW, DR, SR1, SR2, SR3, SR4, ST1, ST2, ST3, ST4, /*S
     input [4:0]RegW; // one-hot encoding for writes
     input [4:0] DR, SR1, SR2, SR3, SR4;//, SR5, SR6, SR7, SR8; // for reading regs
     input [4:0] ST1, ST2, ST3, ST4; // for writing back the result of the ALU
-    input [127:0] Reg_wr_In, Reg_wb1, Reg_wb2, Reg_wb3, Reg_wb4;
+    input [31:0] Reg_wr_In, Reg_wb1, Reg_wb2, Reg_wb3, Reg_wb4;
 
-    input str;
+    input [4:0]str;
 
-    output reg [127:0] ReadReg1, ReadReg2, ReadReg3, ReadReg4;//, ReadReg5, ReadReg6, ReadReg7, ReadReg8;
+    output reg [31:0] ReadReg1, ReadReg2, ReadReg3, ReadReg4;//, ReadReg5, ReadReg6, ReadReg7, ReadReg8;
     
-    output reg [127:0] Reg_str_out;
+    output reg [31:0] Reg_str_out;
 
-    reg [127:0] REG [0:31]; //needs to be 128 bits for VLEN = 128
+    reg [31:0] REG [0:31]; //needs to be 128 bits for VLEN = 128
     integer i;
 
     //always block for reset + write
@@ -47,6 +47,7 @@ module Vec_RF (clk, reset, RegW, DR, SR1, SR2, SR3, SR4, ST1, ST2, ST3, ST4, /*S
         ReadReg2 = REG[SR2];
         ReadReg3 = REG[SR3];
         ReadReg4 = REG[SR4];
+        Reg_str_out = REG[str];
         // ReadReg5 = REG[SR5];
         // ReadReg6 = REG[SR6];
         // ReadReg7 = REG[SR7];
--- hdl/vector_core.v
+++ hdl/vector_core.v
@@ -3,24 +3,40 @@ module Vec_Main (
     input rst,
     input [W_ALUOP-1:0]aluop,
     input [W_VECOP-1:0]vecop,
+    input  [W_DATA-1:0]    d_imm,
+	input  [W_REGADDR-1:0] d_rs1,
+	input  [W_REGADDR-1:0] d_rs2,
+	input  [W_REGADDR-1:0] d_rd,
+	input  [2:0]           d_funct3_32b,
+	input  [6:0]           d_funct7_32b,
+    input  [10:0]   	   d_zimm,
+	input  [W_VECOP-1:0]   d_vecop,
 
-    output [127:0] d_Reg_str_out, // for memory interface
+    output [31:0] d_Reg_str_out, // for memory interface
     output store128
     
 );
 
     reg todo; // if there is a task to do
 
+// loading inputs
+    wire vm = d_funct7_32b[0];
+    wire [1:0] mop = d_funct7_32b[2:1];
+    wire mew = d_funct7_32b[3];
+    wire [2:0] nf = d_funct7_32b[6:4];
+
+
+
 
     //wire RegW;
     wire [4:0]RegW; // one-hot encoding for writes
     wire [4:0] DR, SR1, SR2, SR3, SR4;//, SR5, SR6, SR7, SR8; // for reading regs
     wire [4:0] ST1, ST2, ST3, ST4; // for writing back the result of the ALU
-    wire [127:0] Reg_wr_In, Reg_wb1, Reg_wb2, Reg_wb3, Reg_wb4;
-    wire [127:0] ReadReg1, ReadReg2, ReadReg3, ReadReg4;//, ReadReg5, ReadReg6, ReadReg7, ReadReg8;
+    wire [31:0] Reg_wr_In, Reg_wb1, Reg_wb2, Reg_wb3, Reg_wb4;
+    wire [31:0] ReadReg1, ReadReg2, ReadReg3, ReadReg4;//, ReadReg5, ReadReg6, ReadReg7, ReadReg8;
 
-    wire str;
-    wire [127:0] Reg_str_out;
+    wire [4:0]str;
+    wire [31:0] Reg_str_out;
 
     Vec_RF VRF(clk, rst,RegW, DR, SR1, SR2, SR3, SR4, ST1, ST2, ST3, ST4, /*SR5, SR6, SR7, SR8,*/ Reg_wr_In, Reg_wb1, Reg_wb2, Reg_wb3, Reg_wb4, str, ReadReg1, ReadReg2, ReadReg3, ReadReg4/*, ReadReg5, ReadReg6, ReadReg7, ReadReg8*/,Reg_str_out);
 
