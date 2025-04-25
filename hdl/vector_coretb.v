adbb7f2ce2f0eaeac4093f836e1e925ab02c28bc
kmarzouq <marzouq.mujtaba@gmail.com>
2025-04-25 12:23:05 -0700

    3x3 load

--- hdl/vector_coretb.v
+++ hdl/vector_coretb.v
@@ -141,11 +141,11 @@ module testbench #(
         //d_funct3_32b = 3'b000; // width = 8 bits
         //d_funct3_32b = 3'b101; // width = 16 bits
         d_funct3_32b = 3'b110; // width = 32 bits
-        d_funct7_32b = 7'b001_0_00_1; // [31:29] nf , [28] mew, [27:26] mop , [25]vm
+        d_funct7_32b = 7'b010_0_00_1; // [31:29] nf , [28] mew, [27:26] mop , [25]vm
         d_vecop = 4'h2; // Vector Load
         vstart = 32'h0000_0000;
         vxrm = 2'b10; // Round down (truncate)
-        vl = 32'd4; // Vector length
+        vl = 32'd3; // Vector length
         vtype = 32'b0_0000000_00000000_00000000_0_0_000_000; // [31]vill , [7]vma,[6]vta,[5:3]vsew,[2:0]vlmul
         vlenb = 32'd16; // 16 8 bit elements
 
@@ -238,6 +238,16 @@ module testbench #(
         bus_aph_ready_d = 0;
         bus_dph_ready_d = 1;
         bus_rdata_d = 32'h22222222;
+        #10;
+        bus_dph_ready_d = 0;
+        #40
+
+                #10;
+        bus_aph_ready_d = 1;
+        #10
+        bus_aph_ready_d = 0;
+        bus_dph_ready_d = 1;
+        bus_rdata_d = 32'h33333333;
         #10;
         bus_dph_ready_d = 0;
         #40
