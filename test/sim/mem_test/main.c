ea8f44fe00b8b0ca1c6864e6ca9d80aeaa2fbc3b
Tamnac <iberoo33@gmail.com>
2025-04-27 01:51:15 -0700

    mem/config test: integrate asm into c

--- test/sim/mem_test/main.c
+++ test/sim/mem_test/main.c
@@ -1,12 +1,25 @@
-#include "tb_cxxrtl_io.h"
+// #include "tb_cxxrtl_io.h"
 
-extern void memt(int a[10]);
+// extern void memt(int a[10]);
 
 int main() {
-	tb_puts("Hello world from Hazard3 + CXXRTL!\n");
-	int a[10];
+	// RISC-V assembly inline
+	asm volatile (
+		"li x6, 6;"
+		"li x7, 0;"
+		"nop;"
+		"vsetvli	 x5,x6,e32,m1,ta,ma;"
+		"nop;"
+		"vsetvl x5, x6, x7;"
+		"nop;"
+		"vsetivli x5,4,e32,m1,ta,ma;"
+	);
 
-	// tb_put_u32(&a);
-	memt(a);
+
+	// tb_puts("Hello world from Hazard3 + CXXRTL!\n");
+	// int a[10];
+
+	// // tb_put_u32(&a);
+	// memt(a);
 	return 0;
 }
