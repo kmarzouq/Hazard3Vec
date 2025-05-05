bf16b1c7f5987d5d6a62ee7a03af0a89394475a9
Tamnac <iberoo33@gmail.com>
2025-05-04 17:28:20 -0700

    1 cycle overhead (throughput) for loads

--- hdl/vector_core.v
+++ hdl/vector_core.v
@@ -303,11 +303,6 @@ always @(*) begin // address generation per register to iterate through
                 ld_str_addrs[i] = 0;
             end
     end
-    // else if (todo & no_todo) begin
-    //     for (i = 0; i < 128; i=i+1) begin 
-    //             ld_str_addrs[i] = 0;
-    //         end
-    // end
     else if (d_vecop == VECOP_LOAD | d_vecop == VECOP_STORE) begin
         case (mop)
             UNIT_STRIDE: begin //loading 32-bits at a time. no point for striding
@@ -382,12 +377,12 @@ reg [3:0] pos_to_load [511:0]; // position in register to load to
 
 integer i2;
 always @(*) begin // target register generation
-    if(!rst_n | (todo==1 & no_todo==1)) begin // rst at start of new vector instruction
+/*     if(!rst_n | (todo==1 & no_todo==1)) begin // rst at start of new vector instruction
             for (i2 = 0; i2 < 128; i2 = i2+1) begin 
                 reg_to_load[i2] = 0;
             end
     end
-    else if (d_vecop == VECOP_LOAD | d_vecop==VECOP_STORE) begin
+    else */ if (d_vecop == VECOP_LOAD | d_vecop==VECOP_STORE) begin
         for (i2 = 0; i2 < 128; i2 = i2+1) begin //assuming vl = VLMAX = 128
 
             case (vlmul) // finding register to load to 
@@ -465,12 +460,15 @@ reg [7:0] skip_cntr_ld; //for NF when vl < VLEN/EEW*NF
 
 reg ld_write;
 
-wire [127:0] ld_gap_maker,ld_gap;//holds register data w/ gap for data to be put in
-wire [127:0] ld_fill;//holds data loaded and ready to be put into gaps
+reg [31:0] bus_data; // avoid reading in garbage
+always_latch @* if (d_vecop == VECOP_LOAD) bus_data = bus_rdata_d;
 
-assign ld_fill = ( (128'd0 | (bus_rdata_d & to_mask)) << (curr_ld_pos*(EEW))); 
+wire [VLEN-1:0] ld_gap_maker,ld_gap;//holds register data w/ gap for data to be put in
+wire [VLEN-1:0] ld_fill;//holds data loaded and ready to be put into gaps
+wire [127:0] prev_bypass = ld_st_reg_delayed == ld_reg_wire_rd ? to_store : 0; // todo will this cause issues with consecutive instr
+assign ld_fill = ( (128'd0 | (bus_data & to_mask)) << (curr_ld_pos*(EEW))); 
 assign ld_gap_maker = ~( (128'd0 | (to_mask) ) << (curr_ld_pos*(EEW)) );
-assign ld_gap = (ReadReg2 & ld_gap_maker);
+assign ld_gap = (prev_bypass & ld_gap_maker); // bypass for 1 cycle pipelined loads
 
 always @* begin
     case (EEW)
@@ -483,168 +481,62 @@ end
 
 reg ld_done;
 reg [8:0] index;
+
+reg [4:0] ld_st_reg_delayed;
 always @(posedge clk or negedge rst_n) begin
-    if (!rst_n | ld_done) begin //waiting for instruction
-        next_ld_addr<=0;
-        next_ld_reg<=0;
-        next_ld_pos<=0; 
-        curr_ld_addr=0;
-        curr_ld_reg=0;
-        curr_ld_pos<=0;
-        to_store<=0;
-        ld_state<=0;
-        passed_len_ld=0;
-        bus_aph_req_d<=0;
-        ld_done<=0;
-        skip_cntr_ld<=0;
-        index=0;
-        ld_write<=0;
+    if (!rst_n | ld_done) begin
+        curr_ld_addr <= 0;
+        curr_ld_reg <= 0;
+        curr_ld_pos <= 0;
+        passed_len_ld = 0;
+        bus_aph_req_d <= 0;
+        ld_done <= 0;
+        skip_cntr_ld <= 0;
+        index <= 0;
+        ld_write <= 0;
     end
-    // else if (ld_state == 0 & d_vecop == VECOP_LOAD) begin // modify to take into account AHB bus
-    //     if ((todo==1 & no_todo==1))begin //needed for syncing w/ address,reg, and position pregeneration
-    //         ld_state<=1;//instruction received "send load request state" / "start state"
-    //     end
-    //     ld_write<=0;
-    // end
-    else if ((ld_state==0 || ld_state == 1)  && d_vecop == VECOP_LOAD) begin //unit stride
-        ld_write<=0;
+    else if (d_vecop == VECOP_LOAD) begin
         case (mop)
             2'b00, 2'b01: begin
-                if (ld_state == 0) begin
-                curr_ld_addr=ld_str_addrs[passed_len_ld];
-
-                curr_ld_reg=reg_to_load[index];
-
-                curr_ld_pos<=pos_to_load[index];
-                next_ld_addr<=ld_str_addrs[passed_len_ld+1];
-                next_ld_reg<=reg_to_load[index+1];
-                next_ld_pos<=pos_to_load[index+1];
-                end
+                // Setup memory request
+                ld_reg_wire_st <= reg_to_load[passed_len_ld];
+                ld_reg_wire_rd <= reg_to_load[passed_len_ld];
+                curr_ld_pos <= pos_to_load[passed_len_ld];
                 
-                ld_reg_wire_st<=curr_ld_reg;
-                ld_reg_wire_rd<=curr_ld_reg;
+                bus_priv_d <= 1;
+                bus_hwrite_d <= 0;
+                bus_aph_excl_d <= 0;
+                bus_wdata_d <= 0;
+                bus_aph_req_d <= 1;
+                bus_haddr_d <= ld_str_addrs[passed_len_ld];
 
-                bus_priv_d<=1; // user mode
-                bus_hwrite_d<=0; // read transaction
-                bus_aph_excl_d<=0; // not exclusive
-                bus_wdata_d<=0; // not storing data
-
-                bus_aph_req_d<=1;//requesting data
-                bus_haddr_d<=curr_ld_addr; // address to read from
-
-                ld_state <= 4; //unit stride
-            end
-            // 2'b01:begin 
-            //     ld_state<=2; //strided
-            //     curr_ld_addr=ld_str_addrs[passed_len_ld];
-            //     curr_ld_reg<=reg_to_load[index];
-            //     curr_ld_pos<=pos_to_load[index];
-            //     next_ld_addr<=ld_str_addrs[passed_len_ld+1];
-            //     next_ld_reg<=reg_to_load[index+1];
-            //     next_ld_pos<=pos_to_load[index+1];
-            //     bus_aph_req_d<=1;//requesting data
-            //     bus_haddr_d<=curr_ld_addr; // address to read from
-            // end 
-            2'b10:ld_state<=11; //indexed
-            2'b11:ld_state<=11; //indexed 
-            default:ld_state<=2; //unit stride
-        endcase
-    end
-    // else if (ld_state==2) begin
-    //     if ((num_elements_LS)==passed_len_ld ) ld_done<=1;
-    //     ld_write<=0;
-    //     //if ((d_rs2 == 5'b00000) | (d_rs2 == US_WLD) | (d_rs2 == US_fault) | (mop == 2'b01)) begin
-    //             bus_aph_req_d<=1;//requesting data
-    //             bus_haddr_d<=curr_ld_addr; // address to read from
-    //             case (EEW)
-    //                 8:bus_hsize_d<=3'd000; // 8-bit | setting size of data load 
-    //                 16:bus_hsize_d<=3'd001; // 16-bit | setting size of data load
-    //                 32:bus_hsize_d<=3'd010; // 32-bit | setting size of data load
-    //                 default:bus_hsize_d<=3'd000; // 8-bit | setting size of data load  
-    //             endcase
-    //             bus_priv_d<=1; // user mode
-    //             bus_hwrite_d<=0; // read transaction
-    //             bus_aph_excl_d<=0; // not exclusive
-    //             bus_wdata_d<=0; // not storing data
-    //             ld_state<=3;
-    //             ld_reg_wire_st<=curr_ld_reg;
-    //             ld_reg_wire_rd<=curr_ld_reg;
-    //             //end
-    // end
-
-    //end
-
-    // else if (ld_state==3) begin
-    //     if (bus_aph_ready_d==1) begin // acknowledgement of request from memory
-    //         ld_state<=4;
-    //         bus_aph_req_d<=0;
-
-    //     end
-    // end
-
-    else if (ld_state==4) begin //load state for unit-stride
-        if (bus_dph_ready_d==1) begin
-            bus_aph_req_d<=0;
-            // ld_write<=1;
-
-            //if (d_rs2 == 5'b00000) begin
-                    if (~mask_en | (mask_en && (mask[passed_len_ld]))) begin // 
+                // Handle data if ready
+                if (bus_dph_ready_d) begin                    
+                    if (~mask_en | (mask_en && mask[passed_len_ld])) begin
                         ld_write <= 1;
-                        to_store <= (( ld_gap | ld_fill) ) ; // storing data
+                        to_store <= (ld_gap | ld_fill);
+                        ld_st_reg_delayed <= ld_reg_wire_st; // delayed since regfile saves next cycle
                     end
                     else begin
-                        to_store <= ReadReg2; // no change
+                        ld_write <= 0;
+                        to_store <= ReadReg2;
                     end
-                    
+                    if (num_elements_LS == passed_len_ld) begin
+                        ld_done <= 1;
+                    end
+                    passed_len_ld = passed_len_ld + 1;
+                    index <= passed_len_ld + 1 + skip_cntr_ld;
 
-            //end
-            // ld_state<=5;
-            passed_len_ld=passed_len_ld+1;
-            index = passed_len_ld + 1 + skip_cntr_ld;
-
-            curr_ld_addr=next_ld_addr;
-            curr_ld_reg=next_ld_reg;
-            curr_ld_pos<=next_ld_pos;
-            next_ld_addr<=ld_str_addrs[passed_len_ld+1];
-            next_ld_reg <= reg_to_load[passed_len_ld+1];
-            next_ld_pos <= pos_to_load[passed_len_ld+1];
-
-            ld_state <= 1;
-            if ((num_elements_LS)==passed_len_ld ) begin
-                ld_done<=1;
-                // ld_reg_wire_st<=curr_ld_reg;
-                // ld_reg_wire_rd<=curr_ld_reg;
-
-                // // bus_priv_d<=1; // user mode
-                // // bus_hwrite_d<=0; // read transaction
-                // // bus_aph_excl_d<=0; // not exclusive
-                // // bus_wdata_d<=0; // not storing data
-
-                bus_aph_req_d<=1;//requesting data
-                // bus_haddr_d<=curr_ld_addr; // address to read from
+                    bus_haddr_d <= ld_str_addrs[passed_len_ld]; // immediately give next address
+                end
+                else begin
+                    ld_write <= 0;
+                end
             end
-        end
+            // 2'b10, 2'b11: // ... indexed handling ...
+            default: begin end// ... default handling ...
+        endcase
     end
-    // else if (ld_state==5)begin
-    //     ld_write<=1;// write data
-    //     ld_state<=6;
-    // end
-    // else if (ld_state==6) begin 
-    //     ld_write<=0;
-    //     ld_state<=7;
-    //     index <= passed_len_ld + 1 + skip_cntr_ld;
-    // end
-    // else if (ld_state==7) begin // finish loading data
-    //     ld_state<=2; // go back to state 2 to load next data
-    //     // ld_write<=0;
-    //     curr_ld_addr<=next_ld_addr;
-    //     curr_ld_reg<=next_ld_reg;
-    //     curr_ld_pos<=next_ld_pos;
-    //     next_ld_addr<=ld_str_addrs[passed_len_ld+1];
-    //     next_ld_reg <= reg_to_load[passed_len_ld+1];
-    //     next_ld_pos <= pos_to_load[passed_len_ld+1];
-        
-    // end
 end
 
 
@@ -655,7 +547,7 @@ end
 
 //add case for VECOP_ARITH
 assign ld_st_reg_wire_rd = (d_vecop==VECOP_LOAD ) ? ld_reg_wire_rd : 0; //swap 0 for st_reg_wire_rd
-assign ld_st_reg_wire_st = (d_vecop==VECOP_LOAD ) ? ld_reg_wire_st : 0; //swap 0 for st_reg_wire_st
+assign ld_st_reg_wire_st = (d_vecop==VECOP_LOAD ) ? ld_st_reg_delayed : 0; //swap 0 for st_reg_wire_st
     wire  RegW;
 
     assign RegW = ld_write;
@@ -762,7 +654,7 @@ vdiv32u_vv #(.MAX_VECWIDTH(MAX_VECWIDTH), .XLEN(XLEN)) vdivu_vv_inst (
     .S(divu_out)
 );
 
- assign Reg_In = result_vector;
+//  assign Reg_In = result_vector;
 // assign RegW = RegW_a;
 assign DR = DR_a;
 
