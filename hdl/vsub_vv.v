a55ab2adfa620f2d54ad2110594af4f354481c43
niels-holzmann <nielsholzmann@gmail.com>
2025-05-05 14:35:36 -0700

    moved mask to separate file

--- hdl/vsub_vv.v
+++ hdl/vsub_vv.v
@@ -1,15 +1,3 @@
-module mask #(
-    parameter VLEN = 16
-)(
-    input vm, // 1: unmasked, 0: use v0.mask
-    input [VLEN-1:0] v0_mask,
-    output [VLEN-1:0] mask_out
-);
-
-assign mask_out = (vm) ? {VLEN{1'b1}} : v0_mask;
-
-endmodule
-
 module vsub32_vv #( 
     parameter MAX_VECWIDTH=16, //Maximum LMUL-supported vector width, up to VLEN
     parameter XLEN = 32 //variable length XLEN, initially set to 32
