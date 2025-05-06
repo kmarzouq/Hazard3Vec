13b1beaf9fa085fbbc508631746ea54a6fbd6da2
kmarzouq <marzouq.mujtaba@gmail.com>
2025-05-06 00:14:49 -0700

    fix for lmul

--- hdl/vector_coretb.v
+++ hdl/vector_coretb.v
@@ -140,7 +140,7 @@ module testbench #(
         d_aluop = ALUOP_VEC; // Example ALU operation
         d_vecop = VECOP_LOAD; // Vector load
 
-        d_rd = 5'd0; // Destination register
+        d_rd = 5'd1; // Destination register
         d_rs1 = 5'd1;// Source register 1
         d_rs2 = 5'b00000;// Source register 2
         scalar_reg1 = 32'd100;
@@ -149,12 +149,12 @@ module testbench #(
         //d_funct3_32b = 3'b000; // width = 8 bits
         //d_funct3_32b = 3'b101; // width = 16 bits
         d_funct3_32b = 3'b110; // width = 32 bits
-        d_funct7_32b = 7'b010_0_00_1; // [31:29] nf , [28] mew, [27:26] mop , [25]vm
+        d_funct7_32b = 7'b000_0_00_1; // [31:29] nf , [28] mew, [27:26] mop , [25]vm
         d_vecop = 4'h2; // Vector Load
         vstart = 32'h0000_0000;
         vxrm = 2'b10; // Round down (truncate)
-        vl = 32'd3; // Vector length
-        vtype = 32'b0_0000000_00000000_00000000_0_0_000_000; // [31]vill , [7]vma,[6]vta,[5:3]vsew,[2:0]vlmul
+        vl = 32'd8; // Vector length
+        vtype = 32'b0_0000000_00000000_00000000_0_0_000_001; // [31]vill , [7]vma,[6]vta,[5:3]vsew,[2:0]vlmul
         vlenb = 32'd16; // 16 8 bit elements
 
 
@@ -240,15 +240,15 @@ module testbench #(
         bus_dph_ready_d = 0;
         #40
         
-        #10;
-        bus_aph_ready_d = 1;
-        #10
-        bus_aph_ready_d = 0;
-        bus_dph_ready_d = 1;
-        bus_rdata_d = 32'h33333333;
-        #10;
-        bus_dph_ready_d = 0;
-        #40
+        // #10;
+        // bus_aph_ready_d = 1;
+        // #10
+        // bus_aph_ready_d = 0;
+        // bus_dph_ready_d = 1;
+        // bus_rdata_d = 32'h33333333;
+        // #10;
+        // bus_dph_ready_d = 0;
+        // #40
 
         d_vecop = 4'h0;
         #10;
